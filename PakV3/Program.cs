97c2ec6cc5a37c9a305784ea093b77eabb6724bf
keven1229 <keven@kevenidc.com>
2021-02-26 03:14:49 +0800

    Add MultiThread

--- PakV3/Program.cs
+++ PakV3/Program.cs
@@ -4,55 +4,75 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PakV3
 {
     class Program
     {
-        const string logFile = @"E:\JX3Dump\Run.log";
+        const string logFile = @"c:\JX3Dump\Run.log";
         static void Log(string message)
         {
             Console.WriteLine(message);
-            File.AppendAllText(logFile, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}  {message}{Environment.NewLine}");
+            //File.AppendAllText(logFile, $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}  {message}{Environment.NewLine}");
         }
 
         static void Main(string[] args)
         {
             File.WriteAllText(logFile, null);
-            string dumpDir = @"E:\JX3DUMP";
-            FileStream fs = new FileStream(@"E:\JX3\Main\JX3HD\PakV3\TRUNK.DIR", FileMode.Open);
+            string dumpDir = @"c:\JX3DUMP";
+            FileStream fs = new FileStream(@"C:\Kingsoft\JX3HD\PakV3\TRUNK.DIR", FileMode.Open);
             TrunkReader tr = new TrunkReader(fs);
-            PackageManager pm = new PackageManager(@"E:\JX3\Main\JX3HD\PakV3\");
+            PackageManager pm = new PackageManager(@"C:\Kingsoft\JX3HD\PakV3");
             Log($"Total Length = {tr.TotalLength}");
             int totalLengthStrLen = tr.TotalLength.ToString().Length;
             int i = 0;
             PackageDataDecompressor decomp = new PackageDataDecompressor();
             Stopwatch sw = new Stopwatch();
             sw.Restart();
-            while(true)
+            int maxThreads = 32;
+            List<Thread> threadList = new List<Thread>();
+
+            bool dumpedToEnd = false;
+            while(!dumpedToEnd)
             {
-                try
+                if (threadList.Count <= maxThreads)
                 {
-                    int packageId = 0;
-                    TrunkAddress item = tr.ReadNextResource();
-                    string name = item.Token.ToString("X16");
-                    PackageData data = new PackageData(pm.GetData(item.Offset, item.Length, out packageId));
-                    byte[] content = decomp.GetDecompressedData(data.CompressType, data.RawData, data.DecompressedLength);
-                    string extension = JX3FileTypeRecognizer.GetExtension(content);
-                    DirectoryInfo dir = new DirectoryInfo($"{dumpDir}\\{packageId}");
-                    if (!dir.Exists)
-                        dir.Create();
-                    File.WriteAllBytes($"{dir}\\{name}.{extension}", content);
-                    Log($"{(++i).ToString()} / {tr.TotalLength}    Dumped: {name}.{extension} at 0x{item.Offset.ToString("X")}, source length is {item.Length}, data block length is {data.RawLength}, compress type is {data.CompressType.ToString()}, decompressed length is {data.DecompressedLength}");
+                    Thread thread = new Thread(() =>
+                    {
+                        try
+                        {
+                            int packageId = 0;
+                            TrunkAddress item = tr.ReadNextResource();
+                            string name = item.Token.ToString("X16");
+                            PackageData data = new PackageData(pm.GetData(item.Offset, item.Length, out packageId));
+                            byte[] content = decomp.GetDecompressedData(data.CompressType, data.RawData, data.DecompressedLength);
+                            string extension = JX3FileTypeRecognizer.GetExtension(content);
+                            DirectoryInfo dir = new DirectoryInfo($"{dumpDir}\\{packageId}");
+                            if (!dir.Exists)
+                                dir.Create();
+                            File.WriteAllBytes($"{dir}\\{name}.{extension}", content);
+                            Log($"{(++i).ToString()} / {tr.TotalLength}    Dumped: {name}.{extension} at 0x{item.Offset.ToString("X")}, source length is {item.Length}, data block length is {data.RawLength}, compress type is {data.CompressType.ToString()}, decompressed length is {data.DecompressedLength}");
+                        }
+                        catch (EndOfStreamException ex)
+                        {
+                            dumpedToEnd = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Log($">>> Error: {ex.Message}");
+                        }
+                    });
+                    thread.Start();
+                    threadList.Add(thread);
                 }
-                catch(EndOfStreamException ex)
+                foreach (var thread in threadList)
                 {
-                    break;
-                }
-                catch(Exception ex)
-                {
-                    Log($">>> Error: {ex.Message}");
+                    if (!thread.IsAlive)
+                    {
+                        
+                    }
                 }
             }
             sw.Stop();
--- PakV3/TrunkReader.cs
+++ PakV3/TrunkReader.cs
@@ -10,27 +10,36 @@ namespace PakV3
     class TrunkReader : BinaryReader
     {
         public UInt32 TotalLength { get; private set; }
-
+        private object readerLock = new object();
+        private object addressLock = new object();
         public TrunkReader(Stream stream) : base(stream)
         {
-            this.BaseStream.Seek(0x0C, SeekOrigin.Begin);
-            this.TotalLength = this.ReadUInt32();
-            this.BaseStream.Seek(0x0200, SeekOrigin.Begin);
+            lock (this.readerLock)
+            {
+                this.BaseStream.Seek(0x0C, SeekOrigin.Begin);
+                this.TotalLength = this.ReadUInt32();
+                this.BaseStream.Seek(0x0200, SeekOrigin.Begin);
+            }
         }
 
         public TrunkAddress ReadNextResource()
         {
             byte[] buffer = new byte[20];
-            int read = this.BaseStream.Read(buffer, 0, buffer.Length);
-            if (read < 20)
-                throw new EndOfStreamException("无法读取下一个完整对象, 已经读取到末尾?");
-            TrunkAddress address = new TrunkAddress()
+            lock (this.addressLock)
             {
-                Token = BitConverter.ToUInt64(buffer.Take(8).Reverse().ToArray(), 0),
-                Offset = BitConverter.ToInt64(buffer.Skip(8).Take(8).ToArray(), 0),
-                Length = BitConverter.ToInt32(buffer.Skip(16).Take(4).ToArray(), 0)
-            };
-            return address;
+                int read = this.BaseStream.Read(buffer, 0, buffer.Length);
+                if (read < 20)
+                    throw new EndOfStreamException("无法读取下一个完整对象, 已经读取到末尾?");
+
+                TrunkAddress address = new TrunkAddress()
+                {
+                    Token = BitConverter.ToUInt64(buffer.Take(8).Reverse().ToArray(), 0),
+                    Offset = BitConverter.ToInt64(buffer.Skip(8).Take(8).ToArray(), 0),
+                    Length = BitConverter.ToInt32(buffer.Skip(16).Take(4).ToArray(), 0)
+                };
+
+                return address;
+            }
         }
 
     }
